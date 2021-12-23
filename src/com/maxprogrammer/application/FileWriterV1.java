48fd1cefdb1075257a66aa9b342eb59379d35a67
maxProgrammer <maxwneto.poo@gmail.com>
2021-12-23 17:23:18 -0300

    manipulando arquivos e pastas com scanner, fileReader, bufferedReader e buffereadWriter

---
+++ src/com/maxprogrammer/application/FileWriterV1.java
@@ -0,0 +1,21 @@
+package com.maxprogrammer.application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterV1 {
+    public static void main(String[] args) {
+        String[] lines = new String[]{"Good morning","Good afternoon", "Good evening"};
+        String path = "c:\\temp\\out.txt";
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))){
+            for (String line: lines){
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/maxprogrammer/application/FolderManager.java
@@ -0,0 +1,36 @@
+package com.maxprogrammer.application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FolderManager {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a folder path:");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        File[] folders = path.listFiles(File::isDirectory);
+
+        System.out.println("FOLDERS:");
+
+        for(File folder: folders){
+            System.out.println(folder);
+        }
+
+        System.out.println("------------------");
+
+        File[] files = path.listFiles(File::isFile);
+        System.out.println("FILES:");
+        for(File file: files){
+            System.out.println(file);
+        }
+
+        //cria um subdiretório
+        boolean sucess = new File(strPath + "\\subdir").mkdir();
+        System.out.println("Directory created successfully: " + sucess);
+
+    }
+}
